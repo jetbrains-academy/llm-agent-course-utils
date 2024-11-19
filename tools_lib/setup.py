f702e961914232db8541a99aa2cb6c300593b9f2
RodionfromHSE <rodionkhvorostov@gmail.com>
2024-11-19 11:30:01 +0100

    add tools_basics lib

---
+++ tools_lib/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tools_basics",  # The package name users will use to install
+    version="0.1.0",
+    packages=find_packages(where="src"),  # Look for packages in the "src" folder
+    package_dir={"": "src"},  # Map root of the package to "src"
+    install_requires=[],  # Add dependencies here if needed
+    description="A set of tools for lesson 01_basics",
+    author="Rodion Khvorostov",
+    author_email="rodion.khvorostov@jetbrains.com",
+)
---
+++ tools_lib/src/tools_basics/helpers.py
@@ -0,0 +1,21 @@
+import numpy as np
+import os
+from omegaconf import OmegaConf
+from typing import Iterable
+
+
+DEFAULT_CONF_PATH = os.path.join(os.path.dirname(__file__), "../" * 3, "conf.yaml")
+def get_config(path: str = DEFAULT_CONF_PATH) -> OmegaConf:
+    """Get the configuration from the YAML file.
+    Set `root_dir` to the directory of the config file.
+    
+    Note: The function expects the conf.yaml file to be in the root of the project."""
+    conf_raw = OmegaConf.load(path)
+    root_dir = os.path.dirname(path)
+    conf_raw.root_dir = os.path.abspath(root_dir)
+    conf = OmegaConf.create(OmegaConf.to_yaml(conf_raw, resolve=True))
+    return conf
+
+def save_as_numpy(data: Iterable, path: str) -> None:
+    """Save the data as a NumPy file."""
+    np.save(path, np.array(data))
